c9779009b3d45ea06cd700566929407ee5c70ff7
JOHN LAWLESS - STUDENT <G00351835@gmit.ie>
2020-02-03 09:37:26 +0000

    First commit to new Calculator repository

---
+++ src/main/java/ie/gmit/Calculator.java
@@ -0,0 +1,47 @@
+package ie.gmit;
+
+public class Calculator {
+
+    private double num1;
+    private double num2;
+    private double result;
+
+    public Calculator() {
+        num1 = 0.0;
+        num2 = 0.0;
+    }
+
+    public Calculator(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+
+    public double getNum1() {
+        return num1;
+    }
+
+    public void setNum1(double num1) {
+        this.num1 = num1;
+    }
+
+    public double getNum2() {
+        return num2;
+    }
+
+    public void setNum2(double num2) {
+        this.num2 = num2;
+    }
+
+
+    public double addNumbers(double num1, double num2){
+        result =  num1 + num2;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Calculator : " +
+                + num1 +
+                " " + num2 + " = " + result;
+    }
+}
